dc4119ae847d9291a1d3a0529dcd3a27ac0b562e
LAPTOP-UV5JFG6L\Elodie <elodie.noele@ensta-bretagne.org>
2018-04-03 16:45:16 -0400

    no message

--- ProjetINF8225.py
+++ ProjetINF8225.py
@@ -14,42 +14,145 @@ des liens ici:
 
  Pour ce qui est des tags NLTK, je les laisse ici :
     POS tag list:
-
-    CC coordinating conjunction
-    CD cardinal digit
-    DT determiner
-    EX existential there (like: "there is" ... think of it like "there exists")
-    FW foreign word
-    IN preposition/subordinating conjunction
-    JJ adjective 'big'
-    JJR adjective, comparative 'bigger'
-    JJS adjective, superlative 'biggest'
-    LS list marker 1)
-    MD modal could, will
-    NN noun, singular 'desk'
-    NNS noun plural 'desks'
-    NNP proper noun, singular 'Harrison'
-    NNPS proper noun, plural 'Americans'
-    PDT predeterminer 'all the kids'
-    POS possessive ending parent's
-    PRP personal pronoun I, he, she
-    PRP$ possessive pronoun my, his, hers
-    RB adverb very, silently,
-    RBR adverb, comparative better
-    RBS adverb, superlative best
-    RP particle give up
-    TO to go 'to' the store.
-    UH interjection errrrrrrrm
-    VB verb, base form take
-    VBD verb, past tense took
-    VBG verb, gerund/present participle taking
-    VBN verb, past participle taken
-    VBP verb, sing. present, non-3d take
-    VBZ verb, 3rd person sing. present takes
-    WDT wh-determiner which
-    WP wh-pronoun who, what
-    WP$ possessive wh-pronoun whose
-    WRB wh-abverb where, when
+    
+        $: dollar
+        $ -$ --$ A$ C$ HK$ M$ NZ$ S$ U.S.$ US$
+    '': closing quotation mark
+        ' ''
+    (: opening parenthesis
+        ( [ {
+    ): closing parenthesis
+        ) ] }
+    ,: comma
+        ,
+    --: dash
+        --
+    .: sentence terminator
+        . ! ?
+    :: colon or ellipsis
+        : ; ...
+    CC: conjunction, coordinating
+        & 'n and both but either et for less minus neither nor or plus so
+        therefore times v. versus vs. whether yet
+    CD: numeral, cardinal
+        mid-1890 nine-thirty forty-two one-tenth ten million 0.5 one forty-
+        seven 1987 twenty '79 zero two 78-degrees eighty-four IX '60s .025
+        fifteen 271,124 dozen quintillion DM2,000 ...
+    DT: determiner
+        all an another any both del each either every half la many much nary
+        neither no some such that the them these this those
+    EX: existential there
+        there
+    FW: foreign word
+        gemeinschaft hund ich jeux habeas Haementeria Herr K'ang-si vous
+        lutihaw alai je jour objets salutaris fille quibusdam pas trop Monte
+        terram fiche oui corporis ...
+    IN: preposition or conjunction, subordinating
+        astride among uppon whether out inside pro despite on by throughout
+        below within for towards near behind atop around if like until below
+        next into if beside ...
+    JJ: adjective or numeral, ordinal
+        third ill-mannered pre-war regrettable oiled calamitous first separable
+        ectoplasmic battery-powered participatory fourth still-to-be-named
+        multilingual multi-disciplinary ...
+    JJR: adjective, comparative
+        bleaker braver breezier briefer brighter brisker broader bumper busier
+        calmer cheaper choosier cleaner clearer closer colder commoner costlier
+        cozier creamier crunchier cuter ...
+    JJS: adjective, superlative
+        calmest cheapest choicest classiest cleanest clearest closest commonest
+        corniest costliest crassest creepiest crudest cutest darkest deadliest
+        dearest deepest densest dinkiest ...
+    LS: list item marker
+        A A. B B. C C. D E F First G H I J K One SP-44001 SP-44002 SP-44005
+        SP-44007 Second Third Three Two * a b c d first five four one six three
+        two
+    MD: modal auxiliary
+        can cannot could couldn't dare may might must need ought shall should
+        shouldn't will would
+    NN: noun, common, singular or mass
+        common-carrier cabbage knuckle-duster Casino afghan shed thermostat
+        investment slide humour falloff slick wind hyena override subhumanity
+        machinist ...
+    NNP: noun, proper, singular
+        Motown Venneboerger Czestochwa Ranzer Conchita Trumplane Christos
+        Oceanside Escobar Kreisler Sawyer Cougar Yvette Ervin ODI Darryl CTCA
+        Shannon A.K.C. Meltex Liverpool ...
+    NNPS: noun, proper, plural
+        Americans Americas Amharas Amityvilles Amusements Anarcho-Syndicalists
+        Andalusians Andes Andruses Angels Animals Anthony Antilles Antiques
+        Apache Apaches Apocrypha ...
+    NNS: noun, common, plural
+        undergraduates scotches bric-a-brac products bodyguards facets coasts
+        divestitures storehouses designs clubs fragrances averages
+        subjectivists apprehensions muses factory-jobs ...
+    PDT: pre-determiner
+        all both half many quite such sure this
+    POS: genitive marker
+        ' 's
+    PRP: pronoun, personal
+        hers herself him himself hisself it itself me myself one oneself ours
+        ourselves ownself self she thee theirs them themselves they thou thy us
+    PRP$: pronoun, possessive
+        her his mine my our ours their thy your
+    RB: adverb
+        occasionally unabatingly maddeningly adventurously professedly
+        stirringly prominently technologically magisterially predominately
+        swiftly fiscally pitilessly ...
+    RBR: adverb, comparative
+        further gloomier grander graver greater grimmer harder harsher
+        healthier heavier higher however larger later leaner lengthier less-
+        perfectly lesser lonelier longer louder lower more ...
+    RBS: adverb, superlative
+        best biggest bluntest earliest farthest first furthest hardest
+        heartiest highest largest least less most nearest second tightest worst
+    RP: particle
+        aboard about across along apart around aside at away back before behind
+        by crop down ever fast for forth from go high i.e. in into just later
+        low more off on open out over per pie raising start teeth that through
+        under unto up up-pp upon whole with you
+    SYM: symbol
+        % & ' '' ''. ) ). * + ,. < = > @ A[fj] U.S U.S.S.R * ** ***
+    TO: "to" as preposition or infinitive marker
+        to
+    UH: interjection
+        Goodbye Goody Gosh Wow Jeepers Jee-sus Hubba Hey Kee-reist Oops amen
+        huh howdy uh dammit whammo shucks heck anyways whodunnit honey golly
+        man baby diddle hush sonuvabitch ...
+    VB: verb, base form
+        ask assemble assess assign assume atone attention avoid bake balkanize
+        bank begin behold believe bend benefit bevel beware bless boil bomb
+        boost brace break bring broil brush build ...
+    VBD: verb, past tense
+        dipped pleaded swiped regummed soaked tidied convened halted registered
+        cushioned exacted snubbed strode aimed adopted belied figgered
+        speculated wore appreciated contemplated ...
+    VBG: verb, present participle or gerund
+        telegraphing stirring focusing angering judging stalling lactating
+        hankerin' alleging veering capping approaching traveling besieging
+        encrypting interrupting erasing wincing ...
+    VBN: verb, past participle
+        multihulled dilapidated aerosolized chaired languished panelized used
+        experimented flourished imitated reunifed factored condensed sheared
+        unsettled primed dubbed desired ...
+    VBP: verb, present tense, not 3rd person singular
+        predominate wrap resort sue twist spill cure lengthen brush terminate
+        appear tend stray glisten obtain comprise detest tease attract
+        emphasize mold postpone sever return wag ...
+    VBZ: verb, present tense, 3rd person singular
+        bases reconstructs marks mixes displeases seals carps weaves snatches
+        slumps stretches authorizes smolders pictures emerges stockpiles
+        seduces fizzes uses bolsters slaps speaks pleads ...
+    WDT: WH-determiner
+        that what whatever which whichever
+    WP: WH-pronoun
+        that what whatever whatsoever which who whom whosoever
+    WP$: WH-pronoun, possessive
+        whose
+    WRB: Wh-adverb
+        how however whence whenever where whereby whereever wherein whereof why
+    ``: opening quotation mark
+        ` ``
 
 """
 from nltk import word_tokenize as wt, pos_tag as pt
@@ -63,6 +166,53 @@ import torch.nn.functional as F
 import torchvision.transforms as transforms
 from torch import nn
 from torch import optim
+
+# Definition du dictionnaire. Chaque entite grammaticale est associee a un nombre.
+pos_tag_dict = {0 : "CC",
+                1 : "CD",
+                2 : "DT",
+                3 : "EX",
+                4 : "FW",
+                5 : "IN",
+                6 : "JJ",
+                7 : "JJR",
+                8 : "JJS",
+                9 : "LS",
+                10: "MD",
+                11: "NN",
+                12: "NNS",
+                13: "NNP",
+                14: "NNPS",
+                15: "PDT",
+                16: "POS",
+                17: "PRP",
+                18: "PRP$",
+                19: "RB",
+                20: "RBR",
+                21: "RBS",
+                22: "RP",
+                23: "TO",
+                24: "UH",
+                25: "VB",
+                26: "VBD",
+                27: "VBG",
+                28: "VBN",
+                29: "VBP",
+                30: "VBZ",
+                31: "WDT",
+                32: "WP",
+                33: "WP$",
+                34: "WRB",
+                35: "''",
+                36: "$",
+                37: "(",
+                38: ")",
+                39: ",",
+                40: "--",
+                41: ".",
+                42: ":"}
+
+# Mise en forme du corpus 
 
 def prepare_sequence(seq, to_ix):
     idxs = [to_ix[w] for w in seq]
